098fdcd16e16e723317c8e1dbb17360a0774ba90
Andrezz122 <andrez4403@gmail.com>
2025-06-09 14:08:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -935,7 +935,7 @@
   </div>
 </div>
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if(isset($_POST['Registrar'])){
 $nombre=$_POST['nombre'];
 $apellido=$_POST['apellido'];
 $edad=$_POST['edad'];
