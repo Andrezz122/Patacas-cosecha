ca2adcf0130add2aa41f67a4fb2eaf2b97512405
Andrezz122 <andrez4403@gmail.com>
2025-06-09 13:03:19 -0500

    Update procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,17 +1,3 @@
-<?php
-
-$servername="localhost";
-$username="root";
-$password="";
-$database="";
-
-$conect = mysqli_connect($servername, $username, $password, $database);
-
-if (!$conect) {
-	die("conexion fallida: " .mysqli_connect_error());
-}
-?>
-
 
 <?php
 $servername = "localhost";
