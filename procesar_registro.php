71961f9e40797407486a1b5e39855aea20d16c74
Andrezz122 <andrez4403@gmail.com>
2025-06-09 13:11:22 -0500

    Update procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -5,7 +5,7 @@ $database = "tienda_juegos";
 $username = "root";
 $password = "";
 
-$conect = mysqli_connect($servername, $database, $username, $password);
+$conect = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conect) {
 	die("conexion fallida: " .mysqli_connect_error());
