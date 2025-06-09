e0ac55b877a83017b5a373192b9b2cae9795d9f1
Andrezz122 <andrez4403@gmail.com>
2025-06-09 13:00:52 -0500

    Create procesar_registro.php

---
+++ procesar_registro.php
@@ -0,0 +1,13 @@
+<?php
+procesar_registro.php
+$servername="localhost";
+$username="root";
+$password="";
+$database="tienda_juegos";
+
+$conect = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conect) {
+	die("conexion fallida: " .mysqli_connect_error());
+}
+?>
